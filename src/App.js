d6be832e89e3abbdfba642f4a15e3a03cca85b97
Indrajith V <indrajithvgp@yahoo.com>
2021-02-03 14:37:23 +0530

    updated menu items to navigate withRouter HOC

--- src/App.js
+++ src/App.js
@@ -1,11 +1,21 @@
 import './App.css';
 import HomePage from './pages/homepage.component.jsx'
+import {Switch, Route} from 'react-router-dom'
 
+const HatsPage = (props)=>{
+  console.log(props)
+  return(
+    <div><h1>Hola</h1></div>
+  )  
+}
 
 function App() {
   return (
     <div>
-      <HomePage/>
+      <Switch>
+        <Route exact path='/' component={HomePage}/>
+        <Route exact path='/hats' component={HatsPage}/>
+      </Switch>     
     </div>
   );
 }
--- src/components/directory/directory.component.jsx
+++ src/components/directory/directory.component.jsx
@@ -1,6 +1,6 @@
 import React, {Component} from 'react'
 import './directory.styles.scss'
-import {MenuItem} from '../menu-item/menu-item.component'
+import MenuItem from '../menu-item/menu-item.component'
 
 class Directory extends Component {
 
@@ -10,28 +10,33 @@ class Directory extends Component {
             sections:[{
                 title:'HATS',
                 imageUrl:'https://i.ibb.co/cvpntL1/hats.png',
-                id:1
+                id:1,
+                linkUrl:'hats'
             },{
                 title:'JACKETS',
                 imageUrl:'https://i.ibb.co/px2tCc3/jackets.png',
-                id:2
+                id:2,
+                linkUrl:''
             },{
                 title:'SNEAKERS',
                 imageUrl:'https://i.ibb.co/0jqHpnp/sneakers.png',
-                id:3
+                id:3,
+                linkUrl:''
             },{
                 title:'WOMENS',
                 imageUrl:'https://i.ibb.co/GCCdy8t/womens.png',
                 id:4,
-                size:'large'
+                size:'large',
+                linkUrl:''
             },
             {
                 title:'MENS',
                 imageUrl:'https://i.ibb.co/R70vBrQ/mens.png',
                 id:5,
-                size:'large'
+                size:'large',
+                linkUrl:''
             }
-        ]
+        ] 
         }
     }
 
@@ -39,8 +44,8 @@ class Directory extends Component {
         return(
             <div className="directory-menu">
             {
-                this.state.sections.map(({title, imageUrl, id, size}) =>(
-                    <MenuItem key={id} title={title} imageUrl={imageUrl} size={size}/>
+                this.state.sections.map(({id, ...otherSectionProps}) =>(
+                    <MenuItem key={id} {...otherSectionProps}/>
                 ))
             }
             
