bc78f11d0cc8f14f9f5a880a5ef75b7106566d3b
Indrajith V <indrajithvgp@yahoo.com>
2021-02-03 11:19:44 +0530

    added homepage content and styles

---
+++ src/homepage.component.jsx
@@ -0,0 +1,33 @@
+import React from 'react'
+import './homepage.style.scss'
+
+
+
+const HomePage=()=> (
+
+        <div className="homepage">
+            <div className="directory-menu">
+                <div className="menu-item">
+                    <div className="content">
+                        <h1 className="title">HATS</h1>
+                        <span className="subtitle">SHOP NOW</span>
+                    </div>
+                </div>
+                <div className="menu-item">
+                    <div className="content">
+                        <h1 className="title">WINTER WEAR</h1>
+                        <span className="subtitle">SHOP NOW</span>
+                    </div>
+                </div>
+                <div className="menu-item">
+                    <div className="content">
+                        <h1 className="title">SPORTS WEAR</h1>
+                        <span className="subtitle">SHOP NOW</span>
+                    </div>
+                </div>
+            </div> 
+        </div>
+)
+
+
+export default HomePage
