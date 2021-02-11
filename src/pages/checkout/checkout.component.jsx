325c7cf07f639b8d47fc89bb5c956748b0de36f2
Indrajith V <indrajithvgp@yahoo.com>
2021-02-11 13:46:32 +0530

    finished checkout page with quantity addidtion and removal functionality

--- src/pages/checkout/checkout.component.jsx
+++ src/pages/checkout/checkout.component.jsx
@@ -28,7 +28,7 @@ const CheckoutPage=({cartItems,total})=> {
             </div>
             {cartItems.map(cartItem => <CheckoutItem key={cartItem.id} cartItem={cartItem}/> )}
             <div className="total">
-                <span>TOTAL: {total} USD</span>
+                <span>TOTAL: ${total} </span>
             </div>
         </div>
     )
--- src/redux/cart/cart.reducer.js
+++ src/redux/cart/cart.reducer.js
@@ -1,5 +1,6 @@
 import CartActionTypes from './cart.types'
-import {addItemToCart} from './cart.utils'
+import {addItemToCart, removeItemFromCart} from './cart.utils'
+// import {clearItemFromCart} from './cart.utils'
 
 
 const INITIAL_STATE = {
@@ -19,6 +20,20 @@ const cartReducer = (state= INITIAL_STATE, action)=>{
                 ...state,
                 cartItems: addItemToCart(state.cartItems, action.payload)
             }
+
+        case CartActionTypes.CLEAR_ITEM_FROM_CART:
+            return{
+                ...state,
+                // cartItems: clearItemFromCart(state.cartItems, action.payload)
+                cartItems: state.cartItems.filter(cartItem =>cartItem.id!==action.payload.id)
+            }
+
+        case CartActionTypes.REMOVE_ITEM:
+            return{
+                ...state,
+                cartItems: removeItemFromCart(state.cartItems, action.payload)
+            }
+
         default:
             return state
     }
